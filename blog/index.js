74b2dc62700ad12a80e73114d26e485c83b682f0
Vossie <sv7337@outlook.com>
2021-06-11 12:11:09 -0400

    established json packages, express and started handlebars

---
+++ blog/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const handlebars = require("express-handlebars")
+const routes = require("./routes");
+const app = express();
+app.engine("handlebars", handlebars());
+app.set ("view engine", "handlebars")
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", routes);
+
+app.listen(3000);
+
